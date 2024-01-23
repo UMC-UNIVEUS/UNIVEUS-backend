8068ac8768ed021780323c42a7c8d45f840c725e
ReadMyMemory <lame3925@gmail.com>
2024-01-23 14:10:09 +0900

    Fix : userProfileDTO return 값 수정

--- src/app/profile/profileResponseDTO.js
+++ src/app/profile/profileResponseDTO.js
@@ -3,9 +3,7 @@ import { response, errResponse, baseResponse } from "../../../config/response";
 export const userProfileDTO = async(UserProfileResponse, user_id) => {
     const changeClassof = Math.floor(UserProfileResponse.class_of / 100000 % 100);
     UserProfileResponse.class_of = changeClassof + "학번"
-
-    const Response = {SuccessDTO, UserProfileResponse}
-    return Response;
+    return response(baseResponse.SUCCESS, UserProfileResponse);
 }
 
 export const userIntroductionDTO = async(type, userIntroductionResponse) => {
