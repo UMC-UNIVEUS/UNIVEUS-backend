b6c621148f0d68f26700d8d765db7ffef793d1b0
ReadMyMemory <lame3925@gmail.com>
2024-01-23 14:45:07 +0900

    Remove : 축제 이전 기존 코드 제거

--- src/app/profile/profileRoute.js
+++ src/app/profile/profileRoute.js
@@ -2,22 +2,15 @@
 
 import express from 'express';
 import {
-    //getUserProfile,
-    //putUserProfile,
-    getUserMyUnive,
-    getUserParticipate, getUserProfile, postUserIntroduction
+    getUserIntroduction,
+    postUserIntroduction, putUserIntroduction
 } from "./profileController";
 import { jwtMiddleware } from "../../../config/jwtMiddleWare";
 import {wrapAsync} from "../../../config/errorhandler";
 const profileRouter = express.Router();
 
-//profileRouter.get('/profile/:user_id', getUserProfile);
-//profileRouter.put('/profile/:user_id', putUserProfile);
 profileRouter.post('/introduction',jwtMiddleware , postUserIntroduction);
-profileRouter.get('/',jwtMiddleware, wrapAsync(getUserProfile));
-// profileRouter.get('/myunive', jwtMiddleware, wrapAsync(getUserMyUnive));
-// profileRouter.get('/participate', jwtMiddleware, wrapAsync(getUserParticipate));
-// profileRouter.get('/myProfile', jwtMiddleware, wrapAsync(getUserMyUnive));
-profileRouter.get('/userProfile',jwtMiddleware, wrapAsync(getUserMyUnive));
+profileRouter.get('/introduction',jwtMiddleware , getUserIntroduction);
+profileRouter.put('/introduction',jwtMiddleware , putUserIntroduction);
 
 export default profileRouter;
