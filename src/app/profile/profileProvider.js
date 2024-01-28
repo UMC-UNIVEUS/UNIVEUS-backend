2bb1ad64ace6f54e3e39f3b9c8bc663952eaa17a
ReadMyMemory <lame3925@gmail.com>
2024-01-28 18:44:23 +0900

    Fix : code lexical declaration error 해결

--- src/app/profile/profileProvider.js
+++ src/app/profile/profileProvider.js
@@ -19,10 +19,7 @@ dayjs.extend(duration);
 export const retrieveUserIntroduction = async(userId) => {
     const connection = await pool.getConnection(async (conn) => conn);
     const retrieveUserInfoResult = await selectUserInfo(connection, userId);
-    if(retrieveUserInfoResult == null)
-        const retrieveUserIntroductionResult = null;
-    else
-        const retrieveUserIntroductionResult = await selectUserIntroduction(connection, userId);
+    const retrieveUserIntroductionResult = await selectUserIntroduction(connection, userId);
     connection.release();
     return {retrieveUserInfoResult, retrieveUserIntroductionResult};
 }
