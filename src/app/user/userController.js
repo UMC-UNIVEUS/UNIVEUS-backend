72b4b4a70dc3c09736d3dcce08dd900b6f616a42
임채연 <dlacodus0407@gmail.com>
2024-02-06 17:03:06 +0900

    Refactoring : 로그인 로직 변경 서버에서 인가토큰 요청하는 방식으로 변경

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -73,7 +73,7 @@ export const loginRedirect = async(req, res) => {
 
     console.log("들어옴")
 
-    const GOOGLE_LOGIN_REDIRECT_URI = 'http://localhost:3000/user/login/redirect';
+    const GOOGLE_LOGIN_REDIRECT_URI = 'https://univeus.site/login';
     const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
     const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo';
 
@@ -92,6 +92,7 @@ export const loginRedirect = async(req, res) => {
             redirect_uri: GOOGLE_LOGIN_REDIRECT_URI,
             grant_type: 'authorization_code',
         });
+
         const resp2 = await axios.get(GOOGLE_USERINFO_URL, {
           headers: {
               Authorization: `Bearer ${resp.data.access_token}`,
